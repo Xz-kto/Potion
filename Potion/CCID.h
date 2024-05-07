8b78297431ed28c41e5ba5899fef89455601ecb5
Xz-kto <ru.otstavnojnikita.ru@gmail.com>
2024-05-08 02:19:11 +0500

    4 - 6

--- Potion/CCID.h
+++ Potion/CCID.h
@@ -81,3 +81,169 @@ public:
 	void GetDrink() { wcout << L"Применили зелье регенерации"; }
 	void GetCook() { wcout << L"Сварили зелье левитации"; }
 };
+
+class PotionContainerIterator : public Iterator<Potion*>
+{
+private:
+	Potion** PotionBox;
+	int Count;
+	int Pos;
+
+public:
+	PotionContainerIterator(Potion** potionBox, int count)
+	{
+		PotionBox = potionBox;
+		Count = count;
+		Pos = 0;
+	}
+
+	void First() { Pos = 0; }
+	void Next() { Pos++; }
+	bool IsDone() const { return Pos >= Count; }
+	Potion* GetCurrent() const { return PotionBox[Pos]; }
+};
+
+class PotionContainer
+{
+private:
+	Potion** PotionBox;
+	int PotionCount;
+	int MaxSize;
+	
+public:
+	PotionContainer(int maxSize);
+	virtual ~PotionContainer();
+	void AddPotion(Potion* newPotion);
+	int GetCount() const { return PotionCount; }
+	Potion* GetByIndex(int index) const { return PotionBox[index]; }
+
+	Iterator<Potion*>* GetIterator()
+	{
+		return new PotionContainerIterator(PotionBox, PotionCount);
+	}
+};
+
+class VectorPotionBoxIterator : public Iterator<Potion*>
+{
+private:
+	vector<Potion*> PotionBox;
+	int Count;
+	int Pos;
+
+public:
+	VectorPotionBoxIterator(vector<Potion*> potionBox, int count)
+	{
+		PotionBox = potionBox;
+		Count = count;
+		Pos = 0;
+	}
+	void First() { Pos = 0; }
+	void Next() { Pos++; }
+	bool IsDone() const { return Pos >= Count; }
+	Potion* GetCurrent() const { return PotionBox[Pos]; }
+};
+
+class VectorPotionCantainer
+{
+private:
+	vector<Potion*> PotionBox;
+	int Count;
+
+public:
+	void AddPotion(Potion* newPotion) { PotionBox.push_back(newPotion); }
+	int GetCount() const { return PotionBox.size(); }
+	Potion* GetByIndex(int index) const { return PotionBox[index]; }
+
+	Iterator<Potion*>* GetIterator()
+	{
+		return new VectorPotionBoxIterator(PotionBox, Count);
+	}
+};
+
+class PotionTypeDecorator : public IteratorDecorator<Potion*>
+{
+private:
+	PotionType TargetType;
+public:
+	PotionTypeDecorator(Iterator<Potion*>* it, PotionType targetType) : IteratorDecorator(it)
+	{
+		TargetType = targetType;
+	}
+
+	void First()
+	{
+		It->First();
+		while (!It->IsDone() && It->GetCurrent()->GetType() != TargetType)
+		{
+			It->Next();
+		}
+	}
+
+	void Next()
+	{
+		do
+		{
+			It->Next();
+		}
+		while (!It->IsDone() && It->GetCurrent()->GetType() != TargetType);
+	}
+};
+
+class PotionEffectDecorator : public IteratorDecorator<Potion*>
+{
+private:
+	PotionEffect TargetEffect;
+public:
+	PotionEffectDecorator(Iterator<Potion*>* it, PotionEffect targetEffect) : IteratorDecorator(it)
+	{
+		TargetEffect = targetEffect;
+	}
+
+	void First()
+	{
+		It->First();
+		while (!It->IsDone() && It->GetCurrent()->GetEffect() != TargetEffect)
+		{
+			It->Next();
+		}
+	}
+
+	void Next()
+	{
+		do
+		{
+			It->Next();
+		}
+		while (!It->IsDone() && It->GetCurrent()->GetEffect() != TargetEffect);
+	}
+};
+
+class PotionClassDecorator : public IteratorDecorator<Potion*>
+{
+private:
+	PotionClass TargetClass;
+public:
+	PotionClassDecorator(Iterator<Potion*>* it, PotionClass targetClass) : IteratorDecorator(it)
+	{
+		TargetClass = targetClass;
+	}
+
+	void First()
+	{
+		It->First();
+		while (!It->IsDone() && It->GetCurrent()->GetClass() != TargetClass)
+		{
+			It->Next();
+		}
+	}
+
+	void Next() {
+		do 
+		{
+			It->Next();
+		} 
+		while (!It->IsDone() && It->GetCurrent()->GetClass() != TargetClass);
+	}
+};
+
+#endif
